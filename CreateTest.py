c588e309763f47cfde28ab9453e36cdfd1527485
fobostm <fobostm@gmail.com>
2017-07-08 23:06:49 +0300

    init

---
+++ CreateTest.py
@@ -0,0 +1,14 @@
+import subprocess
+import timeit
+
+beg = timeit.default_timer()
+
+for i in range(1000):
+    try:
+        id = subprocess.call('notepad', timeout = 0)
+    except:
+        pass
+
+with open('g:/time.txt', 'w') as out:
+    time = timeit.default_timer() - beg
+    out.write(str(time))
